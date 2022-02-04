13f57966a40930d97c5656efe60f3fdc15271635
CaptainCat911 <dimonax33@gmail.com>
2022-02-04 17:05:51 +0300

    Делал фонари (управление ими)

---
+++ Assets/_Scripts/Game_Zombie_Atack/LampControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampControl : MonoBehaviour
+{
+    public Animator anim;
+    public bool lightDamaged = false;
+    public bool lightGood;
+    
+
+    void Start()
+    {
+        if (lightGood)
+            return;
+        int random = Random.Range(0, 100);
+        if (random > 95 || lightDamaged)
+        {
+            anim.SetBool("LightDamaged", true);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            anim.SetBool("LightOff", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            anim.SetBool("LightOff", true);
+        }
+    }
+}
