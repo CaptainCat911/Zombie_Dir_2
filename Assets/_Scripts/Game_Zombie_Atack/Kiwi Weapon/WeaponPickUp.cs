43a6c2499b13fc4b983df0d95d00a5487c87271e
CaptainCat911 <dimonax33@gmail.com>
2022-04-10 18:30:00 +0300

    Делал гранату
    Эффекты нифига не работают, добавить счётчик гранат

---
+++ Assets/_Scripts/Game_Zombie_Atack/Kiwi Weapon/WeaponPickUp.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class WeaponPickUp : MonoBehaviour
+{
+    public RaycastWeapon weaponPrefab;
+    bool triggerEnter = false;
+    ActiveWeapon activeWeapon;
+
+
+    void Start()
+    {
+        activeWeapon = GameManager.instance.player.GetComponent<ActiveWeapon>();
+
+    }
+
+
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.name == "Player_Soldier")
+            triggerEnter = true;
+    }
+
+    public void OnTriggerExit(Collider collision)
+    {
+        if (collision.name == "Player_Soldier")
+            triggerEnter = false;
+    }
+
+
+
+
+    private void Update()
+    {
+        if (triggerEnter)
+        {
+            RaycastWeapon newWeapon = Instantiate(weaponPrefab);
+            activeWeapon.GetWeaponUp(newWeapon);
+            Destroy(gameObject);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            RaycastWeapon newWeapon = Instantiate(weaponPrefab);
+            activeWeapon.GetWeaponUp(newWeapon);
+            Destroy(gameObject);
+        }
+    }
+}
